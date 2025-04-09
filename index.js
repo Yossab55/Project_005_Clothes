30d5954bccee97b5570709e261c03146e28d628a
Yossab_Samuel <yossabpro5@gmail.com>
2025-04-09 10:15:36 +0200

    add code to run the server

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import "dotenv/config.js";
-import express from "express";
+import express, { response } from "express";
 import mongoose from "mongoose";
 import {env} from "./source/helpers.js";
 
 
 const app = express();
-await mongoose.connect(env("DB_CONNECTION"))
-app.listen(env("PORT"))
+mongoose.connect(env("DB_CONNECTION")).then(() => {
+  app.listen(env("PORT"), () => {
+    console.log(`server is listen on port: ${env("PORT")}`);
+  });
+})
+
+const db = mongoose.connection;
+
+db.on("err", () => {
+  console.log("error with db");
+})
+db.on("open", () => {
+  console.log("db is open");
+})
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+import { ItemsRouter } from "./routers/ItemsRouter.js";
+app.use("/items", ItemsRouter)
+
+// app.get("/", (request, response) => {
+//   response.redirect("/items")
+// })
---
+++ source/Code.js
@@ -0,0 +1,8 @@
+const Code = {
+  GOOD: 200,
+  REDIRECT: 301,
+  NOT_FOUND: 404,
+  SERVER_DOWN: 500
+}
+
+export {Code};
