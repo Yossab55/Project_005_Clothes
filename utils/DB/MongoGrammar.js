08c60895deaafb8fee206f55f5530f08c079f7eb
Yossab_Samuel <yossabpro5@gmail.com>
2025-06-03 09:49:18 +0300

    I guess this is the last thing I would do

--- utils/DB/MongoGrammar.js
+++ utils/DB/MongoGrammar.js
@@ -1,11 +1,13 @@
-import mongoose from "mongoose";
 import { getObjectId } from "../getObjectId.js";
 class MongoGrammar {
   static updateGrammar(oldData, data = {}, id) {
     const filter = { _id: getObjectId(id) };
+    const fields = Object.keys(oldData).filter(
+      (field) => !["_id", "_v"].includes(field)
+    );
     let updateFiled = {};
-    Object.entries(data).forEach(([felid, value]) => {
-      updateFiled[felid] = value || oldData.felid;
+    Object.entries(data).forEach(([field, value]) => {
+      if (fields.includes(field)) updateFiled[field] = value || oldData.felid;
     });
     const update = { $set: updateFiled };
     return { filter, update };
