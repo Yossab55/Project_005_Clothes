9ef67721d9f3283fe7a7b9a532cf08067b638fc0
Yossab_Samuel <yossabpro5@gmail.com>
2025-06-11 16:16:13 +0300

    remove password confimration function in helprs file

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,20 +1,5 @@
-import bcrypt from "bcrypt";
-import { AppError } from "./AppError.js";
-import { ERROR_CODE_PASSWORD_CONFIRMATION } from "./constant/errorCode.js";
-import { RESPONSE_CODE_BAD } from "./constant/responseCode.js";
 function env(prop) {
   return process.env[prop] || undefined;
 }
 
-// async function passwordConfirmation(oldPassword, userPassword) {
-//   const check = await bcrypt.compare(oldPassword, userPassword);
-//   if (!check)
-//     throw new AppError(
-//       ERROR_CODE_PASSWORD_CONFIRMATION,
-//       "You're password is wrong",
-//       RESPONSE_CODE_BAD
-//     );
-//   return check;
-// }
-
-export { env /*passwordConfirmation*/ };
+export { env };
