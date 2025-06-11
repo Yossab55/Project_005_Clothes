f0a8885577c3dcc3d94e01c255fcf804919beb7f
Yossab_Samuel <yossabpro5@gmail.com>
2025-06-11 18:46:12 +0300

    refactor error handler to be 2 lines of code and be more cleaner

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,38 +1,7 @@
-import { AppError } from "../utils/errors/AppError.js";
-import { ERROR_CODE_UNIQUE } from "../utils/constant/errorCode.js";
-import {
-  RESPONSE_CODE_BAD,
-  RESPONSE_CODE_SERVER_DOWN,
-} from "../utils/constant/responseCode.js";
-import { deleteFileFrom } from "../utils/fileHandle.js";
-import { validationError } from "../utils/errors/validationError.js";
+import { ErrorHandleAbstraction } from "../utils/errors/ErrorHandleAbstraction.js";
 async function errorHandler(error, req, res, next) {
-  // console.log(error);
-  //# code Error handel
-  if (error.code == ERROR_CODE_UNIQUE) {
-    res.status(RESPONSE_CODE_BAD).json({
-      errors: {
-        email: "this email is already exists",
-      },
-    });
-  }
-
-  //# validation error
-
-  if (
-    error.message.includes("Validation") ||
-    error.message.includes("validation")
-  ) {
-    validationError.call(error, req, res);
-  }
-
-  //#AppErrorHandel
-  if (error instanceof AppError) {
-    error.validation(res);
-  }
-  if (!res.headersSent) {
-    res.status(RESPONSE_CODE_SERVER_DOWN).json({ error });
-  }
+  const valid = new ErrorHandleAbstraction(error, req, res, next);
+  valid.handleError();
 }
 
 export { errorHandler };
---
+++ utils/errors/ErrorHandleAbstraction.js
@@ -0,0 +1,56 @@
+import { AppError } from "./AppError.js";
+import { ERROR_CODE_UNIQUE } from "../constant/errorCode.js";
+import {
+  RESPONSE_CODE_BAD,
+  RESPONSE_CODE_SERVER_DOWN,
+} from "../constant/responseCode.js";
+import { validationError } from "./validationError.js";
+class ErrorHandleAbstraction {
+  constructor(error, req, res, next) {
+    this.error = error;
+    this.req = req;
+    this.res = res;
+    this.next = next;
+  }
+  handleError() {
+    if (this.errorCodeValidation()) return;
+    if (this.validationErrorHandle()) return;
+    if (this.appErrorHandle()) return;
+    this.serverErrorHandle();
+  }
+  errorCodeValidation() {
+    if (this.error.code == ERROR_CODE_UNIQUE) {
+      this.res.status(RESPONSE_CODE_BAD).json({
+        errors: {
+          email: "this email is already exists",
+        },
+      });
+      return true;
+    }
+    return false;
+  }
+  validationErrorHandle() {
+    if (
+      this.error.message.includes("Validation") ||
+      this.error.message.includes("validation")
+    ) {
+      validationError.call(this.error, this.req, this.res);
+      return true;
+    }
+    return false;
+  }
+  appErrorHandle() {
+    if (this.error instanceof AppError) {
+      this.error.validation(this.res);
+      return true;
+    }
+    return false;
+  }
+  serverErrorHandle() {
+    if (!this.res.headersSent) {
+      const error = this.error;
+      this.res.status(RESPONSE_CODE_SERVER_DOWN).json({ error });
+    }
+  }
+}
+export { ErrorHandleAbstraction };
