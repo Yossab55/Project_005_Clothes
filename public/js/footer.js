7f2514ac1f3eca7813b14c4cbde67b41a84f58d0
Yossab_Samuel <yossabpro5@gmail.com>
2025-06-28 10:18:18 +0300

    refactor(footer.js): make the function more simple and small

--- public/js/footer.js
+++ public/js/footer.js
@@ -5,19 +5,20 @@ footerLinks.forEach((link) => {
 });
 function addActiveClass(element) {
   const parentElement = element.target.parentElement;
-  footerLinks.forEach((link) => {
-    if (link == parentElement) return;
-    if (link.classList.contains(CLASS_ACTIVE_LINK)) {
-      link.classList.remove("active-link");
-      console.log(link.children[0]);
-      const icon = link.children[0].classList.item(1);
-      const iconWithoutFill = icon.split("-").slice(0, -1).join("-");
-      console.log(icon, iconWithoutFill);
-      link.children[0].classList.replace(icon, iconWithoutFill);
-    }
-  });
+  footerLinks.forEach(removeActiveClass, { parentElement });
   parentElement.classList.add(CLASS_ACTIVE_LINK);
   const icon = element.target.classList.item(1);
   const iconFill = icon + "-fill";
   element.target.classList.replace(icon, iconFill);
+}
+function removeActiveClass(link) {
+  if (link == this.parentElement) return;
+  if (link.classList.contains(CLASS_ACTIVE_LINK)) {
+    link.classList.remove("active-link");
+    console.log(link.children[0]);
+    const icon = link.children[0].classList.item(1);
+    const iconWithoutFill = icon.split("-").slice(0, -1).join("-");
+    console.log(icon, iconWithoutFill);
+    link.children[0].classList.replace(icon, iconWithoutFill);
+  }
 }
